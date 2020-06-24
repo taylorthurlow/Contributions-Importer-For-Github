a07e9d0a6b4be9518a349d6cf240238e238a5b12
Taylor Thurlow <taylorthurlow@me.com>
2020-06-23 20:29:08 -0700

    Add more settings

--- script.py
+++ script.py
@@ -2,15 +2,22 @@ import git
 import os
 from git_contributions_importer import *
 
-mock_repo = git.Repo("/Users/taylor/Code/contributions-sync")
+mock_repo = git.Repo("/Users/taylorthurlow/Code/contributions-sync")
 
-for d in next(os.walk("/Users/taylor/Code/bayphoto"))[1]:
+for d in next(os.walk("/Users/taylorthurlow/Code/bayphoto"))[1]:
     print("Starting directory:", d)
-    repo = git.Repo("/Users/taylor/Code/bayphoto/" + d)
+    repo = git.Repo("/Users/taylorthurlow/Code/bayphoto/" + d)
     importer = Importer([repo], mock_repo)
-    importer.set_author(["taylorthurlow8@gmail.com", "taylor.thurlow@bayphoto.com"])
-    importer.set_max_commits_per_day = [99999,99999]
-    importer.set_start_from_last(True)
+    importer.set_author([
+        "taylorthurlow8@gmail.com",
+        "taylor.thurlow@bayphoto.com",
+        "taylorthurlow@me.com",
+        "tthurlow@hey.com",
+    ])
+    importer.set_max_commits_per_day([99999,99999])
+    importer.set_start_from_last(False)
+    importer.set_max_changes_per_file(10000)
+    importer.set_collapse_multiple_changes_to_one(False)
     importer.import_repository()
 
 
